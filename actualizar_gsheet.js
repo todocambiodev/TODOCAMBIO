a65fa0915955f8f669d2afe9a0b9c4a94dc9c209
todocambiodev <todocambiodivisa@gmail.com>
2026-01-06 19:35:52 -0500

    Update actualizar_gsheet.js

--- actualizar_gsheet.js
+++ actualizar_gsheet.js
@@ -224,7 +224,7 @@ async function main() {
 main();
 
 // Punto de salida
-const minutos = 63;
+const minutos = 90;
 setTimeout(() => {
 	activarMonitor = false;
 	console.log("CULMINANDO PROCESO...");
